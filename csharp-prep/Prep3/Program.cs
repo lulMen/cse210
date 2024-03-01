83995785f9f58e95f76be64aaf530a47e511f8e2
ItsMen <mvdo1091@gmail.com>
2024-03-01 18:06:04 -0500

    Completed core challenges for prep3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess;
+
+        do 
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (number != guess);
+        
     }
 }
