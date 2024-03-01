35460025aa4f104c2cd1f749ad82dbd2ea4e465e
ItsMen <mvdo1091@gmail.com>
2024-03-01 15:10:22 -0500

    Completed core challenges for prep2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+
+        Console.Write("What is the grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade =  int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >=70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You received a {letter}. Congratulations! You've passed.");
+        }
+        else
+        {
+            Console.WriteLine($"You received a {letter}. You've failed. Give it another try.");
+        }
     }
 }
