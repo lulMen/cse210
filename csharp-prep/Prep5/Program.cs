de00f9757730d7e65b31272ac0ee4b54cdee8568
ItsMen <mvdo1091@gmail.com>
2024-03-01 23:32:00 -0500

    Completed core challenges for prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            Console.Write($"{name}, the square of your number is {number}");
+        }
+
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
     }
 }
