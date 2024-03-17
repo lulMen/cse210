23af4d1ddb22d03d1a3497c326a3d193d593ca2e
ItsMen <mvdo1091@gmail.com>
2024-03-17 01:59:49 -0400

    completed develop03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string currentBook = "Proverbs";
+        int currentChapter = 3;
+        int currentVerse = 5;
+        int currentEndVerse = 6;
+        string currentText = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
+        int wordsToHide = 4;
+        bool programRunning = true;
+
+        Reference currentReference = new Reference(currentBook, currentChapter, currentVerse, currentEndVerse);
+        Scripture currentScripture = new Scripture(currentReference, currentText);
+
+        do
+        {
+            Console.WriteLine(currentScripture.GetDisplayText() + "\n");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            string input = Console.ReadLine();
+
+            currentScripture.HideRandomWords(wordsToHide);
+
+            if (input == "quit")
+            {
+                programRunning = false;
+            }
+
+            if (currentScripture.IsCompletelyHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(currentScripture.GetDisplayText() + "\n");
+                Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+                _ = Console.ReadLine();
+            }
+
+            Console.Clear();
+
+        } while (programRunning);
     }
 }
